3230e41b79c6477c591b6791e757ab2913259ff5
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-04-07 23:27:50 +0500

    music

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,7 +1,26 @@
 import { Outlet } from "react-router-dom";
 import bg from "../assets/bg.png";
+import { useEffect, useState } from "react";
+import music from "../assets/mc.mp3";
 
 export const Layout = () => {
+  const [audio] = useState(new Audio(music));
+  const [isMusicStarted, setIsMusicStarted] = useState(false);
+  useEffect(() => {
+    const playMusic = () => {
+      if (!isMusicStarted) {
+        audio.loop = true;
+        audio.volume = 0.3;
+        audio.play().catch((err) => console.log("Autoplay blocked:", err));
+        setIsMusicStarted(true);
+      }
+    };
+
+    document.addEventListener("click", playMusic, { once: true });
+
+    return () => document.removeEventListener("click", playMusic);
+  }, [isMusicStarted, audio]);
+
   return (
     <div className="w100 h100 df fdc aic p-r">
       <Outlet />
